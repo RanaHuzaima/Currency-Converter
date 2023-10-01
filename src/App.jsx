886f0205fafd08b16fb5f1a59c032874b3e6721e
Rana Huzaima <huzaimadigital@gmail.com>
2023-10-01 15:48:50 +0500

    add fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CurrencyRow from './Component/CurrencyRow'
 import  './App.css'
 
+const Base_URl = 'https://v6.exchangerate-api.com/v6/7288dc67dc3abe9304365694/latest/usd'
+
 function App() {
+  const [] = useState()
+  useEffect(()=>{
+    fetch(Base_URl)
+    .then(res => res.json())
+    .then(data => console.log(data));
+  },[])
+
   return (
     <div className='main'>
     <h1>Currency Converter</h1>
